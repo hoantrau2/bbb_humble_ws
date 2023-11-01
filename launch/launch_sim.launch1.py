1ca691cd97a838984fe20d54d287d8e076ad0973
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2023-11-01 07:20:12 +0700

    add camera.xacro

---
+++ launch/launch_sim.launch1.py
@@ -0,0 +1,27 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+
+
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+
+
+    # Launch them all!
+    return LaunchDescription([
+        gazebo,
+
+    ])
